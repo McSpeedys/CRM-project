67f8ee221f93d07f4a5621a86981b989997339a5
McSpeedys <eesenogl@asu.edu>
2026-07-02 12:13:53 +0300

    Added login-logout structure and placeholder for admin.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 #Importing flask as this file will handle the initialization of the server.
 from flask import Flask
+#Importing dotenve and using load_dotenv so we can extract the secret key from the .env file.
+from dotenv import load_dotenv
+#Importing os so we can manipulate files on the computer.
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    app.secret_key = os.getenv("SECRET_KEY", "placeholdersecretkey")
 
     from .routes import routes
     app.register_blueprint(routes)
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,61 @@
 #This file will serve to host all the subpages for the website.
-from flask import Blueprint, render_template
+#Importing all necessary packages from flask
+from flask import {
+    Blueprint, 
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+}
 
-routes = Blueprint("routes", __name__)
+#Importing werkzeug for password hashing.
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Temporary admin creds.
+ADMIN ={
+    "username": "admin",
+    "password_hash": ""generate_password_hash("password123")
+}
 
 #At the login page the server will render the login.html file.
 @routes.route("/")
-def login():
+def login_page():
+    if session.get("logged_in"):
+        return redirect(url_for{"routes.dashboard"})
+
     return render_template("login.html")
+
+@routes.route("/login", methods={"POST"})
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    
+    #If username and password match send user to dashboard.
+    if (username == ADMIN["username"] and check_password_hash(ADMIN["password_hash"], password)):
+        session["logged_in"] = True
+        session["username"] = username
+
+        return redirect(url_for("routes.dashboard"))
+    
+    #Else send them back to the login page.
+    return render_template{
+        "login.html",
+        error="Invalid username or password."
+    }
 
 #At the dashboards page the server 
 @routes.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    #If user is not logged in or is trying to skip the login page without logging in send them back.
+    if not session.get("logged_in"):
+        return redirect(url_for("routes.login_page"))
+
+    return render_template("dashboard.html", username=session["username"])
+
+#Logout route
+@routes.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect(url_for("routes.login_page"))
